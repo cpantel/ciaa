6863a89d86b5b8b7baf764964d00e3e7ac18de5f
Carlos Pantelides <carlos.pantelides@gmail.com>
2016-07-26 15:16:54 -0300

    added original HID example

---
+++ slither.io/STANDALONE/usbDeviceHID_keyboard/application/inc/ciaaAIN.h
@@ -0,0 +1,16 @@
+/*
+ * ciaaAIN.h
+ *
+ *  Created on: Jun 24, 2014
+ *      Author: pablo
+ */
+
+#ifndef CIAAAIN_H_
+#define CIAAAIN_H_
+
+#include "chip.h"
+
+void ciaaAINInit(void);
+uint16_t ciaaAINRead(uint8_t input);
+
+#endif /* CIAAAIN_H_ */
