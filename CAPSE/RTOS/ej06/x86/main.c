292ed06fc1eee2adab01cfddb61643e8271396b2
Carlos Pantelides <carlos.pantelides@gmail.com>
2017-08-01 20:07:51 -0300

    ej06 without function pointers

--- CAPSE/RTOS/ej06/x86/main.c
+++ CAPSE/RTOS/ej06/x86/main.c
@@ -1,9 +1,8 @@
 /*
  */
-
+#include <stdio.h>
 /*==================[inclusions]=============================================*/
 
-#include "sapi.h"         /* <= sAPI header */
 
 /*==================[macros and definitions]=================================*/
 
@@ -15,53 +14,37 @@
 
 /*==================[internal data definition]===============================*/
 
-uint8_t task1run = false;
-uint8_t task2run = false;
-uint8_t task3run = false;
+int task1run = 0;
+int task2run = 0;
+int task3run = 0;
 
-uint8_t task1period = 4;
-uint8_t task2period = 2;
-uint8_t task3period = 1;
+int task1period = 4;
+int task2period = 2;
+int task3period = 1;
 
-uint8_t led1 = false;
-uint8_t led2 = false;
-uint8_t led3 = false;
+int led1 = 0;
+int led2 = 0;
+int led3 = 0;
 
 
-uint32_t tickCount = 0;
+int tickCount = 0;
 
 /*==================[external data definition]===============================*/
 
 /*==================[internal functions definition]==========================*/
-void task1init() {
-   gpioConfig(LED1, GPIO_OUTPUT);
-}
-
-
-void task2init() {
-  gpioConfig(LED2, GPIO_OUTPUT);
-}
-
-void task3init() {
-   gpioConfig(LED3, GPIO_OUTPUT);
-
-}
 
 void task1() {
   led1 = !led1;
-  gpioWrite(LED1, led1);
 
 }
 
 void task2() {
   led2 = !led2;
-  gpioWrite(LED2, led2);
 }
 
 
 void task3() {
   led3 = !led3;
-  gpioWrite(LED3, led3);
 }
 
 
@@ -90,23 +73,14 @@ void dispatcher() {
 int main(void){
    /* ------------- INICIALIZACIONES ------------- */
 
-   boardConfig();
-   
-   task1init();
-   task2init();
-   task3init();
+   int t = 0;
+   for(t=0; t< 20; ++t) {
 
-   delay_t delay;
+    scheduler();
+    dispatcher();
+    printf("%d :  %d   %d   %d \n", tickCount, led1, led2, led3);
 
-   delayConfig(&delay, TICK_AMPLITUDE);
-
-   while(1) {
-
-    if (delayRead(&delay)) {
-      scheduler();
-      dispatcher();
-      ++tickCount;      
-    }
+    ++tickCount;      
 
 
    }
