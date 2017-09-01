48befe1a84e74eea358edd105bc52b0f48eef401
Carlos Pantelides <carlos.pantelides@gmail.com>
2017-09-01 11:18:27 -0300

    better enum poc

--- CAPSE/RTOS/POC/enum.c
+++ CAPSE/RTOS/POC/enum.c
@@ -1,4 +1,5 @@
-typedef enum event {  Tec1Pressed,  Tec2Pressed } Events;
+typedef enum alarm {  Alarm1, Alarm2 } Alarms;
+typedef enum event {  Tec1Pressed = 0x01u,  Tec2Pressed = 0x02u } Events;
 typedef enum task  {  Task1, Task2, Task3 } Tasks;
 
 
@@ -10,11 +11,15 @@ void ActivateTask( Tasks task) {
 
 }
 
+void SetRelAlarm (Alarms alarm) {
+
+}
 
 int main(int argc, char* argv[]) {
    WaitEvent(Tec1Pressed);
    WaitEvent(Task1);
    WaitEvent(Task3);
+   SetRelAlarm(Task3);
    return 0;
 }
 
